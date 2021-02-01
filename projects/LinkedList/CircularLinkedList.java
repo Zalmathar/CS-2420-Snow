6b7c0bef288f0aaadb6cd59a076a760a8d797794
zalmathar <jeditanis@gmail.com>
2021-02-01 13:52:04 -0700

    2/1/21 Lecture

---
+++ projects/LinkedList/CircularLinkedList.java
@@ -0,0 +1,22 @@
+// Tanis Olesen
+// 02/01/2021
+// Implements a circularly linked list
+
+public class CircularLinkedList {
+    public class Node {
+        int data;  // Holds the data
+        Node next; // represents the next Node in the circular linked list
+    }
+    // Represents a pointer to the last node in the list
+    Node back;
+
+    // TODO: Constructor
+    public CircularLinkedList() {
+        
+    }
+    // Last pointer in the list points to the front of the list
+    // TODO: pop_back
+    // TODO: pop_front
+    // TODO: empty(), returns true if the list is empty
+    // TODO: 
+}
