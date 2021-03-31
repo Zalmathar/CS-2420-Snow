ca6378de3b35f2a552248efd80386da2d3ec5805
zalmathar <jeditanis@gmail.com>
2021-03-31 09:46:21 -0600

    3/31/21 Pre-Lecture

---
+++ lectures/03/31/notes.java
@@ -0,0 +1,12 @@
+/**
+ * @author  Tanis Olesen
+ *   03/31/2021
+ *   Lecture note's
+ * 
+ */
+public class notes {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ lectures/03/31/treeIterativeDFS/Program.cs
@@ -0,0 +1,62 @@
+﻿// Tanis Olesen
+// 03/29/2021
+// Lecture example and in-class work.
+
+// Depth First Search in a binary tree
+//   - You need to have some sort of stack structure to traverse a tree structure
+//      - This can be done with either a custom stack structure
+//      - Or you can use the runtime stack, and use recursion.
+using System;
+using System.Collections.Generic;
+
+namespace treeIterativeDFS
+{
+    class tree
+    {
+        // Represents a Node inside of a tree structure
+        private class Node
+        {
+            public int? value;
+            public Node left;
+            public Node right;
+
+            public Node(int? value)
+            {
+                this.value = value;
+                left = null;
+                right = null;
+            }
+        }
+        // Represents the root Node in the tree
+        private Node root;
+
+        // Tree Class constructor
+        public void Tree()
+        {
+            root = null;
+        }
+
+        public void TraverseIter()
+        {
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(root);
+            // Console.WriteLine(stack.Peek().value);
+
+            while (stack.Count > 0)
+            {
+                // Console.WriteLine(stack.Pop().value);
+                // // stack.Pop();
+                Node node = stack.Pop();
+                Console.WriteLine(node.value);
+                if (node.left != null)
+                {
+                    stack.Push(node.left);
+                }
+                if (node.right != null)
+                {
+                    stack.Push(node.right);
+                }
+            }
+        }
+    }
+}
